1584bd128405de734b1b821501e3c3ebab5ed7af
Riccardo Marotti <riccardo.marotti@gmail.com>
2019-07-21 17:52:26 +0200

    Minor

--- tests/test_tuples.py
+++ tests/test_tuples.py
@@ -8,8 +8,7 @@ def test_a_point_is_an_array_with_w_set_to_1():
     expected_point = tf.constant(np.array([4.3, -4.2, 3.1, 1.0]))
 
     with tf.Session() as sess:
-        fetches = [actual_point, expected_point]
-        result = sess.run(fetches)
+        result = sess.run([actual_point, expected_point])
 
     assert((result[0] == result[1]).all())
 
@@ -19,7 +18,6 @@ def test_a_vector_is_an_array_with_w_set_to_0():
     expected_vector = tf.constant(np.array([4.3, -4.2, 3.1, 0]))
 
     with tf.Session() as sess:
-        fetches = [actual_vector, expected_vector]
-        result = sess.run(fetches)
+        result = sess.run([actual_vector, expected_vector])
 
     assert((result[0] == result[1]).all())
