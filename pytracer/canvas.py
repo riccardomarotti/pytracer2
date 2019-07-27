0eeb13704636e6853e0ac53a279d974d872470d3
Riccardo Marotti <riccardo.marotti@gmail.com>
2019-07-27 17:12:49 +0200

    Add Canvas.to_PPM()

--- pytracer/canvas.py
+++ pytracer/canvas.py
@@ -3,10 +3,13 @@ import numpy as np
 
 
 class Canvas:
-    def __init__(self, width, height):
+    def __init__(self, width, height, pixels=None):
         self._width = width
         self._height = height
-        self._pixels = tf.zeros((width, height, 3))
+        if pixels is None:
+            self._pixels = tf.zeros((width, height, 3))
+        else:
+            self._pixels = tf.constant(pixels)
 
     @property
     def width(self):
@@ -25,3 +28,34 @@ class Canvas:
         update = tf.SparseTensor(
             indices, [color[0], color[1], color[2]], self.pixels.shape)
         self._pixels = self.pixels + tf.sparse_tensor_to_dense(update)
+
+    def to_PPM(self):
+        ppm_string = "P3\n{} {}\n255\n".format(self.width, self.height)
+
+        with tf.Session() as sess:
+            actualCanvas = sess.run(self.pixels)
+
+        for row_id in range(self.height):
+            current_line = ""
+            colors = actualCanvas[:, row_id]
+            clamped_colors = clamp(colors.flatten())
+            current_line = " ".join(clamped_colors.astype(str))
+            ppm_string += truncate(current_line) + "\n"
+
+        return ppm_string
+
+
+@np.vectorize
+def clamp(color):
+    return int(max(0, min(255, color * 255)))
+
+
+def truncate(s):
+    MAX = 70
+    x = len(s)
+    if(x) < MAX:
+        return s
+
+    last_space_index = s[:MAX+1].rfind(' ')
+    if last_space_index != -1:
+        return s[:last_space_index] + "\n" + s[last_space_index+1:]
--- tests/test_canvas.py
+++ tests/test_canvas.py
@@ -1,6 +1,7 @@
 from pytracer.canvas import Canvas
 from pytracer.colors import color, red
 import tensorflow as tf
+import numpy as np
 
 
 def test_creating_canvas():
@@ -28,3 +29,50 @@ def test_writing_pixels_to_a_canvas():
         actual_colors = sess.run(c.pixels)
 
     assert((actual_colors[2, 3] == (1, 0, 0)).all())
+
+
+def test_constructing_the_PPM_header():
+    actualPPM = Canvas(5, 3).to_PPM()
+
+    expectedPPM = """P3
+5 3
+255"""
+
+    assert(actualPPM.startswith(expectedPPM))
+
+
+def test_constructing_the_PPM_pixel_data():
+    c = Canvas(5, 3)
+    c1 = color(1.5, 0., 0.)
+    c2 = color(0., 0.5, 0.)
+    c3 = color(-0.5, 0., 1.)
+
+    c.add_color_to_pixel(0, 0, c1)
+    c.add_color_to_pixel(2, 1, c2)
+    c.add_color_to_pixel(4, 2, c3)
+
+    actualPPM = c.to_PPM()
+    expectedPPM = """P3
+5 3
+255
+255 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 127 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 255
+"""
+
+    assert(expectedPPM == actualPPM)
+
+
+def test_splitting_long_lines_in_PPM_files():
+    canvas = Canvas(10, 2, np.ones((10, 2, 3))*[1, 0.8, 0.6])
+    actualPPM = canvas.to_PPM()
+
+    expectedPPM = """255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204
+153 255 204 153 255 204 153 255 204 153 255 204 153
+255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204
+153 255 204 153 255 204 153 255 204 153 255 204 153"""
+
+    assert(len(actualPPM.split("\n")) == 8)
+    actualPPM = "\n".join(actualPPM.split("\n")[3:7])
+
+    assert(expectedPPM == actualPPM)
