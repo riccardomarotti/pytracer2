166870cb26160eedd5636d8defe38bf895cc0deb
Riccardo Marotti <riccardo.marotti@gmail.com>
2019-07-28 13:05:42 +0200

    Add scaling

--- pytracer/transformations.py
+++ pytracer/transformations.py
@@ -22,7 +22,16 @@ def translation(x, y, z):
     return lambda p: tf.tensordot(T, p, axes=1)
 
 
+def scaling(x, y, z):
+    T = tf.constant(np.array([
+        [x, 0, 0, 0],
+        [0, y, 0, 0],
+        [0, 0, z, 0],
+        [0, 0, 0, 1]
+    ]),  dtype=tf.float32)
+    return lambda p: tf.tensordot(T, p, axes = 1)
+
 def invert(t):
-    T = t(identity_matrix())
-    Tinv = tf.linalg.inv(T)
+    T=t(identity_matrix())
+    Tinv=tf.linalg.inv(T)
     return lambda p: tf.tensordot(Tinv, p, axes=1)
--- tests/test_transofrmations.py
+++ tests/test_transofrmations.py
@@ -37,3 +37,37 @@ def test_translation_does_not_affect_vectors():
             [expected_vector, transform(expected_vector)])
 
     assert((expected == actual).all())
+
+
+def test_a_scaling_matrix_applied_to_a_point():
+    transform = transformations.scaling(2, 3, 4)
+    p = point(-4, 6, 8)
+    expected_point = point(-8, 18, 32)
+
+    with tf.Session() as sess:
+        [expected, actual] = sess.run([expected_point, transform(p)])
+
+    assert((expected == actual).all())
+
+
+def test_a_scaling_matrix_applied_to_a_vecor():
+    transform = transformations.scaling(2, 3, 4)
+    v = vector(-4, 6, 8)
+    expected_vector = vector(-8, 18, 32)
+
+    with tf.Session() as sess:
+        [expected, actual] = sess.run([expected_vector, transform(v)])
+
+    assert((expected == actual).all())
+
+
+def test_multiplying_the_inverse_of_a_scaling_matrix():
+    transform = transformations.scaling(2, 3, 4)
+    inverse = transformations.invert(transform)
+    v = vector(-4, 6, 8)
+    expected_vector = vector(-2, 2, 2)
+
+    with tf.Session() as sess:
+        [expected, actual] = sess.run([expected_vector, inverse(v)])
+
+    assert((expected == actual).all())
