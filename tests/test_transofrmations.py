27583b3292d05dda7c8c66c89ec849d88a088429
Riccardo Marotti <riccardo.marotti@gmail.com>
2019-07-28 12:16:45 +0200

    Add test for vector translation

--- tests/test_transofrmations.py
+++ tests/test_transofrmations.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 import numpy as np
 from pytracer import transformations
-from pytracer.tuples import point
+from pytracer.tuples import point, vector
 
 
 def test_multiplying__by_a_translation_matrix():
@@ -24,5 +24,16 @@ def test_multiplying_by_the_inverse_of_a_translation_matrix():
 
     with tf.Session() as sess:
         [expected, actual] = sess.run([expected_point, transform(p)])
+
+    assert((expected == actual).all())
+
+
+def test_translation_does_not_affect_vectors():
+    transform = transformations.translation(5, -3, 2)
+    expected_vector = vector(-3, 4, 5)
+
+    with tf.Session() as sess:
+        [expected, actual] = sess.run(
+            [expected_vector, transform(expected_vector)])
 
     assert((expected == actual).all())
